5b7b470c8dde0d34af5527752ae58b2d411f98a0
Ally Milne <ally_milne92@hotmail.com>
2019-06-22 17:29:10 +0200

    * Dynamically adds a button with constraints to exercise card

--- app/src/main/java/com/example/henchmate/ExerciseAdapter.kt
+++ app/src/main/java/com/example/henchmate/ExerciseAdapter.kt
@@ -1,23 +1,31 @@
 package com.example.henchmate
 
 import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
+import android.support.v4.content.ContextCompat.getDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
+import com.example.henchmate.R.drawable.circle_button
+import kotlin.math.roundToInt
 
-class ExerciseAdapter(val context: Context, val exercises: List<Exercise>) : BaseAdapter() {
+class ExerciseAdapter(private val context: Context, private val exercises: List<Exercise>) : BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val exerciseView: View
         val holder: ViewHolder
 
         if (convertView == null) {
-            exerciseView = LayoutInflater.from(context).inflate(R.layout.exercise_card, null)
+            exerciseView = LayoutInflater.from(context).inflate(R.layout.exercise_card, parent, false)
             holder = ViewHolder()
             holder.exerciseName = exerciseView.findViewById(R.id.exerciseHeader)
             holder.setRepInfo = exerciseView.findViewById(R.id.setRepInfo)
+            holder.layout = exerciseView.findViewById(R.id.exerciseCard)
             exerciseView.tag = holder
         } else {
             holder = convertView.tag as ViewHolder
@@ -26,6 +34,34 @@ class ExerciseAdapter(val context: Context, val exercises: List<Exercise>) : Bas
         val exercise = exercises[position]
         holder.exerciseName?.text = exercise.exerciseName
         holder.setRepInfo?.text = "${exercise.numberOfSets}x${exercise.repsPerSet} at ${exercise.weight}kg"
+
+        val btnSet = Button(context)
+        btnSet.id = View.generateViewId()
+        btnSet.text = "5"
+        btnSet.background = getDrawable(context, circle_button)
+        val lp = LinearLayout.LayoutParams(45.dp(context), 45.dp(context))
+        btnSet.layoutParams = lp
+        holder.layout?.addView(btnSet)
+
+        val constraints = ConstraintSet()
+        constraints.clone(holder.layout)
+        constraints.connect(btnSet.id, ConstraintSet.TOP, R.id.exerciseHeader, ConstraintSet.BOTTOM, 16.dp(context))
+        constraints.connect(
+            btnSet.id,
+            ConstraintSet.START,
+            ConstraintSet.PARENT_ID,
+            ConstraintSet.START,
+            16.dp(context)
+        )
+        constraints.connect(
+            btnSet.id,
+            ConstraintSet.BOTTOM,
+            ConstraintSet.PARENT_ID,
+            ConstraintSet.BOTTOM,
+            16.dp(context)
+        )
+
+        constraints.applyTo(holder.layout)
 
         return exerciseView
     }
@@ -42,5 +78,13 @@ class ExerciseAdapter(val context: Context, val exercises: List<Exercise>) : Bas
         return exercises.count()
     }
 
-    private class ViewHolder(var exerciseName: TextView? = null, var setRepInfo: TextView? = null)
+    private fun Int.dp(context: Context): Int {
+        return (this * context.resources.displayMetrics.density).roundToInt()
+    }
+
+    private class ViewHolder(
+        var exerciseName: TextView? = null,
+        var setRepInfo: TextView? = null,
+        var layout: ConstraintLayout? = null
+    )
 }
--- app/src/main/java/com/example/henchmate/MainActivity.kt
+++ app/src/main/java/com/example/henchmate/MainActivity.kt
@@ -6,14 +6,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val squats = Exercise(exerciseName = "Squats", numberOfSets = 5, repsPerSet = 5, weight = 70)
-    val bench = Exercise(exerciseName = "Bench Press", numberOfSets = 5, repsPerSet = 5, weight = 45.5)
-    val bentOverRows = Exercise(exerciseName = "Bent Over Rows", numberOfSets = 5, repsPerSet = 5, weight = 45.5)
+    private val squats = Exercise(exerciseName = "Squats", numberOfSets = 5, repsPerSet = 5, weight = 70)
+    private val bench = Exercise(exerciseName = "Bench Press", numberOfSets = 5, repsPerSet = 5, weight = 45.5)
+    private val bentOverRows = Exercise(exerciseName = "Bent Over Rows", numberOfSets = 5, repsPerSet = 5, weight = 45.5)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        exerciseList.adapter = ExerciseAdapter(this, listOf(squats,bench,bentOverRows))
-
+        exerciseList.adapter = ExerciseAdapter(
+            this, listOf(
+                squats,
+                bench,
+                bentOverRows
+            )
+        )
     }
 }
